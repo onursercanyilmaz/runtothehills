ae125de093076d0ab51cd533577bd1831d111a4e
onursercanyilmaz <onursercanyilmaz@gmail.com>
2024-01-17 01:10:16 +0300

    firebase add get data added

--- src/components/GoogleAuth.tsx
+++ src/components/GoogleAuth.tsx
@@ -1,11 +1,14 @@
+// GoogleAuth.tsx
+
 import React from 'react';
 import { signInWithPopup } from 'firebase/auth';
 import CButton from './CButton';
 import { auth, provider } from '../constants/firebaseConfig';
 import { ArrowEnter16Filled } from "@fluentui/react-icons";
-import { Navigate } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom'; // Move the import statement here
-import createSheet from '../process/GoogleSheetsProcess';
+import { useNavigate } from 'react-router-dom';
+import { addDataToSheet, getUserData } from '../process/GoogleSheetsProcess';
+import { v4 as uuidv4 } from 'uuid';
+
 
 const CLIENT_ID: any = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 const buttonName: string = "login with google";
@@ -13,27 +16,51 @@ const buttonName: string = "login with google";
 interface GoogleAuthProps { }
 
 const GoogleAuth = (props: GoogleAuthProps) => {
-    const navigate = useNavigate(); // Move the declaration here
+    const navigate = useNavigate();
 
     const handleLogin = async () => {
         try {
             const result = await signInWithPopup(auth, provider);
             localStorage.setItem("user", JSON.stringify(result.user));
-            createSheet();
-            //navigate("/");
+            // navigate("/");
         } catch (error) {
             console.log(error);
         }
     };
 
+    const isUserAuthenticated = () => {
+        const userString = localStorage.getItem('user');
+        return !!userString;
+    };
+
+
+    const handleAddNewItem = async () => {
+        const user = JSON.parse(localStorage.getItem("user")!);
+        const data = {
+            id: uuidv4(),
+            pathid: "2",
+            pathname: "2",
+            platformname: "3",
+            itemname: "4",
+            itemlink: "5",
+            progress: "66"
+        };
+        addDataToSheet(user.uid, data);
+        getUserData(user.uid)
+    };
     return (
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-            <CButton style={{ width: '200px', height: "50px" }} buttonName={buttonName} onClick={handleLogin} icon={
-                <img src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA" width={25} alt="Add Folder" />
-            }
+            <CButton
+                style={{ width: '200px', height: "50px" }}
+                buttonName={buttonName}
+                onClick={handleLogin}
+                icon={<ArrowEnter16Filled />}
             />
+
+            <button onClick={handleAddNewItem}>Ekle</button>
         </div>
     );
 };
 
 export default GoogleAuth;
+
--- src/process/GoogleSheetsProcess.tsx
+++ src/process/GoogleSheetsProcess.tsx
@@ -1,104 +1,47 @@
-// createSheet.ts
-import { google, Auth } from 'googleapis';
+import { arrayUnion, doc, getDoc, setDoc } from 'firebase/firestore';
+import { db } from '../constants/firebaseConfig';
 
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
-
-interface Credentials {
-    access_token: string;
-}
-
-const authenticateGoogleSheetsAPI = async (token: string) => {
-    const credentials: Credentials = {
-        access_token: token,
-    };
-
-    const auth = new google.auth.GoogleAuth();
-    auth.fromJSON(credentials as Auth.CredentialBody);
-
-    const sheets = google.sheets({ version: 'v4', auth });
-    return sheets;
-};
-
-const getSheetId = async (sheets: any, title: string) => {
+export const addDataToSheet = async (userUid: any, data: any) => {
     try {
-        // List all spreadsheets
-        const spreadsheetsList = await sheets.spreadsheets.list();
-
-        // Find the sheet by title in each spreadsheet
-        for (const spreadsheet of spreadsheetsList.data.spreadsheets || []) {
-            const spreadsheetId = spreadsheet.spreadsheetId;
-            const response = await sheets.spreadsheets.get({
-                spreadsheetId: spreadsheetId,
-            });
-
-            const sheet = response.data.sheets?.find((sheet: any) => sheet.properties?.title === title);
-
-            if (sheet) {
-                return { spreadsheetId, sheetId: sheet.properties?.sheetId };
-            }
-        }
-
-        return null; // Sheet not found
-    } catch (error: any) {
-        console.error('Error:', error.message);
-        return null; // Return null on error
-    }
-};
-
-const createSheet = async () => {
-    try {
-        // Retrieve the user from localStorage
-        const userString = localStorage.getItem('user');
-        if (!userString) {
-            console.error('User not found in localStorage');
-            return;
-        }
-
-        const user = JSON.parse(userString);
-
-        // Use user's token to authenticate with Google Sheets API
-        const sheets: any = await authenticateGoogleSheetsAPI(user.stsTokenManager.accessToken);
-
         const sheetTitle = 'runtothehillsdata';
-        const sheetInfo = await getSheetId(sheets, sheetTitle);
+        const collectionRef = doc(db, sheetTitle, userUid);
 
-        if (!sheetInfo) {
-            // Sheet not found in any spreadsheet, create a new one
+        // Check if the data already exists in the entries array
+        const snapshot = await getDoc(collectionRef);
+        const existingData = snapshot.exists() ? snapshot.data().entries : [];
 
-            // Specify the headers
-            const headers = ['pathid', 'pathname', 'platformname', 'itemname', 'itemlink', 'progress'];
-
-            // Create the sheet with headers
-            sheets.spreadsheets.create({
-                resource: {
-                    properties: {
-                        title: sheetTitle,
-                    },
-                    sheets: [
-                        {
-                            properties: {
-                                title: 'Sheet1', // Specify sheet name
-                            },
-                            data: [
-                                {
-                                    startRow: 0,
-                                    startColumn: 0,
-                                    rowData: {
-                                        values: headers.map((header) => ({ userEnteredValue: { stringValue: header } })),
-                                    },
-                                },
-                            ],
-                        },
-                    ],
-                },
-            });
+        if (!existingData.includes(data)) {
+            // Data does not exist, add it to the collection
+            await setDoc(collectionRef, { entries: arrayUnion(data) }, { merge: true });
+            console.log(`Data added to Firestore collection for user ${userUid}.`);
         } else {
-            // Sheet found, you can access its info using sheetInfo.spreadsheetId and sheetInfo.sheetId
-            console.log('Sheet found:', sheetInfo);
+            console.log(`Data already exists in Firestore collection for user ${userUid}.`);
         }
     } catch (error: any) {
-        console.error('Error creating Google Sheet:', error.message);
+        console.error('Error adding data to Firestore collection:', error.message);
     }
 };
 
-export default createSheet;
+
+export const getUserData = async (userUid: any) => {
+    try {
+        const sheetTitle = 'runtothehillsdata';
+        const collectionRef = doc(db, sheetTitle, userUid);
+
+        const snapshot = await getDoc(collectionRef);
+
+        if (snapshot.exists()) {
+            const userData = snapshot.data().entries || [];
+            console.log(`User data retrieved for user ${userUid}:`, userData);
+            return userData;
+        } else {
+            console.log(`No data found for user ${userUid}.`);
+            return null;
+        }
+    } catch (error: any) {
+        console.error('Error getting user data from Firestore:', error.message);
+        return null;
+    }
+};
+
+
