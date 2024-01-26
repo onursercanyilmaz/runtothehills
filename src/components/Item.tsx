50ef4060956c0211135d8612fe42b22f8940eadd
onursercanyilmaz <onursercanyilmaz@gmail.com>
2024-01-26 21:56:31 +0300

    inside button alternative

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,35 +1,41 @@
 import React from 'react'
 import CButton from './CButton'
-import { ArrowCircleRight24Filled, ArrowRight24Filled } from "@fluentui/react-icons";
-import { hover } from '@testing-library/user-event/dist/hover';
+import { ArrowRight24Filled } from "@fluentui/react-icons";
 import { Button, Label } from '@fluentui/react-components';
+import './components.css'
 
 export default function Item() {
     return (
         <div style={{ textAlign: "center" }}>
             <CButton
+                className="itemButton"
                 buttonName={
-                    <div style={{ display: "flex" }}>
-                        <img
-                            src="https://play-lh.googleusercontent.com/oDuTGEHru1KMr3QOfQfPKgIdNnlq3WWQxpBYND23r2a7RVnS1HW0t7dyON86Vn_QhtM=w240-h480-rw"
-                            width={50}
-                            height={50}
-                            style={{ borderRadius: "10%" }}
+                    <div style={{ display: "flex", justifyContent: "space-between" }}>
+                        <div style={{ display: "flex" }}>
+                            <img
+                                src="https://play-lh.googleusercontent.com/oDuTGEHru1KMr3QOfQfPKgIdNnlq3WWQxpBYND23r2a7RVnS1HW0t7dyON86Vn_QhtM=w240-h480-rw"
+                                width={50}
+                                height={50}
+                                style={{ borderRadius: "10%" }}
 
-                        />
-                        <div style={{ flexDirection: "column", display: "flex", alignContent: "center", alignSelf: "center", marginLeft: "10px" }}>
-                            <Label style={{ alignSelf: "center", color: "white", fontSize: "20px", }}>
-                                C# .NET Core Clean Architecture & CQRS Proje Altyapı Kursu 4
-                            </Label>
-                            <Label style={{ color: "white", fontSize: "12px", }}>Item Description</Label>
+                            />
+                            <div style={{ flexDirection: "column", display: "flex", alignContent: "center", alignSelf: "center", marginLeft: "10px" }}>
+                                <Label style={{ alignSelf: "center", color: "white", fontSize: "20px", }}>
+                                    C# .NET Core Clean Architecture & CQRS Proje Altyapı Kursu 4
+                                </Label>
+                                <Label style={{ color: "white", fontSize: "12px", }}>Item Description</Label>
+                            </div>
                         </div>
-                        <Label style={{ alignSelf: "center", color: "white", fontSize: "20px", marginLeft: "50px" }}>%23</Label>
+                        <div style={{ display: "flex" }}>
+                            <Label style={{ alignSelf: "center", color: "white", fontSize: "20px" }}>%23</Label>
+                            <Button onClick={() => alert("button clicked")} style={{ backgroundColor: "transparent", border: "none", textAlign: "right", height: "75px", margin: "0px" }}><ArrowRight24Filled /></Button>
+                        </div>
                     </div>
                 }
-                after={<Button style={{ borderLeft: "none", height: "100px", marginTop: "20px", backgroundColor: "#1f1f1f", border: "var(--strokeWidthThin) solid var(--colorNeutralStroke1)" }}><ArrowRight24Filled /></Button>}
+                //after={<Button style={{ borderLeft: "none", height: "100px", marginTop: "20px", backgroundColor: "#1f1f1f", border: "var(--strokeWidthThin) solid var(--colorNeutralStroke1)" }}><ArrowRight24Filled /></Button>}
                 //icon={<AddCircle32Filled />}
                 onClick={() => alert("deneme")}
-                style={{ marginTop: "20px", width: '75%', height: "100px", alignIsems: "left", justifyContent: "left", backgroundColor: "#1f1f1f", }}
+                style={{ paddingRight: "0px", marginTop: "20px", width: '75%', height: "100px", alignIsems: "left", justifyContent: "left", backgroundColor: "#1f1f1f", }}
             />
 
         </div>
